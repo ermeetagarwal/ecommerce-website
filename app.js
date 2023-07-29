763ab4dd5563317f4a0fad5e5bc4d4b2029015cc
ermeetagarwal <meetagarwal507@gmail.com>
2023-07-29 20:03:40 +0530

    removing ejs and adding path packages

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require("path");
 const mongoose = require('mongoose');
-const ejs = require('ejs');
 
 const homeRouter = require("./routes/home");
 
 const app = express();
 
-
-app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public')));
 
-app.use('/',homeRouter);
+app.use('/',path.join(__dirname,homeRouter));
 
 mongoose.connect(process.env.DB_URL,{ useUnifiedTopology: true ,  useNewUrlParser:true});
 
